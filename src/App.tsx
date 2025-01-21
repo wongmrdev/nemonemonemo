f6f1c271e9d7081b8dd28999e6a3f2a33ca5c037
Matt Wong <20716672+wongmrdev@users.noreply.github.com>
2025-01-21 04:55:46 -0500

    add-userPool

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -23,6 +23,7 @@ function App() {
 
   return (
     <main>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
